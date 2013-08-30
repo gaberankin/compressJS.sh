0f34d75c5a07b1d88e31d3848c3f14e77742bfc3
gabe rankin <gabe.rankin@gmail.com>
2013-08-30 15:12:06 -0400

    change that allows for specification of an output file     This is my first time with complex shell scripting.  don't judge too harshly. TODO - add a help option, make the -y option work at any point in the argument list

--- compressjs.sh
+++ compressjs.sh
@@ -1,27 +1,58 @@
 #!/bin/bash
 
-# Constants
 SERVICE_URL=http://closure-compiler.appspot.com/compile
-NEWFILE="c`date +"%d%m%y"`.js"
+nextone=0
+outfile="auto"
+allowforce=0
+code=""
 
-# Check if files to compile are provided
-if [ $# -eq 0 ]
-then
-	echo 'Nothing to compile. Specify input files as command arguments. E.g.'
-	echo './compressjs file1.js file2.js file3.js'
-	exit
+for f in "$@"
+do
+	case $f in
+		'-y' )
+			allowforce=1
+			;;
+		'-o' )
+			nextone='-o'
+			;;
+		* )
+			case $nextone in
+				'-o' )
+					if [ $allowforce = 0 ]; then
+						if [ -e "$f" ]; then
+							echo "$f already exists.  ok to overwrite? [Y/n] "
+							read overwrite
+							if [ $overwrite != 'Y' ]; then
+								echo "file not overwritten.  exiting"
+								exit 1
+							fi
+						fi
+					fi
+					outfile="$f"
+					nextone=0
+					;;
+				* )
+					if [ -r "$f" ]; then
+						code="${code} --data-urlencode js_code@${f}"
+					else
+						echo "$f not found or not readable.  skipping"
+					fi
+					;;
+			esac
+			;;
+	esac
+done
+
+if [ "$code" = "" ]; then
+	echo "no valid files specified.  exiting."
+	exit 1
 fi
 
-# Itearate through all files
-for f in $*
-do
-	if [ -r ${f} ]
-	then
-		code="${code} --data-urlencode js_code@${f}"
-	else
-		echo "File ${f} does not exist or is not readable. Skipped."
-	fi
-done
+if [ "$outfile" = "auto" ]; then
+	outfile="c`date +"%d%m%y"`.js"
+	echo "No output file specified.  compressed files will be saved as $outfile"
+fi
+
 
 # Send request
 curl \
@@ -54,9 +85,13 @@ else:
 	print "Compressed size: %db, gziped: %db" % (data["statistics"]["compressedSize"], data["statistics"]["compressedGzipSize"])
 	print "Compression rate: %.2f" % (float(data["statistics"]["compressedSize"]) / int(data["statistics"]["originalSize"]))
 
-	filename = "'${NEWFILE}'"
+	filename = "'${outfile}'"
 	f = open(filename, "w")
 	f.write(data["compiledCode"])
 
 	print "\nBuild file %s created.\n" % filename
 ' $@
+
+
+echo "$outfile"
+echo "${code}"
